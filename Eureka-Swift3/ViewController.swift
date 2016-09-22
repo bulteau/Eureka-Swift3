39726f0279e1a00741b8478b96759fe00966da07
François Bulteau <fr.bulteau@gmail.com>
2016-09-22 16:06:44 +0200

    Initial Commit

---
+++ Eureka-Swift3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Eureka-Swift3
+//
+//  Created by François Bulteau on 22/09/2016.
+//  Copyright © 2016 François Bulteau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
